a142837727b53d517ba89a76df3a05eb2bff9886
David E. Wheeler <david@justatheory.com>
2021-07-27 13:57:15 -0400

    Remove ohmymup function
    Using ZSH_CUSTOM now, so not necessary anymore.

--- plugins/theory/theory.plugin.zsh
+++ plugins/theory/theory.plugin.zsh
@@ -1,3 +1,6 @@
+# In ~/.zshrc:
+# ZSH_CUSTOM=$HOME/dev/misc/oh-my-zsh-theory
+
 export PATH=$HOME/.plenv/shims:$HOME/.plenv/bin:$HOME/.cargo/bin:$HOME/.pgenv/bin:$HOME/.pgenv/pgsql/bin:$HOME/bin:/usr/local/opt/python/libexec/bin:/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.7.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Developer/Tools:/Applications/Emacs.app/Contents/MacOS
 
 # Move git-path prompt to the left.
@@ -15,17 +18,6 @@ function pmr () { perl -MModule::CoreList -le "print Module::CoreList->first_rel
 # Homebrew stuff
 gettext_init() { export PATH=/usr/local/opt/gettext/bin:$PATH }
 ssl_init()     { export PATH=/usr/local/opt/openssl/bin:$PATH }
-
-# Oh-my-zsh upgrade
-function ohmyup () {
-    cd $ZSH
-    rm -rf custom
-    git checkout .
-    git pull
-    rm -rf custom
-    ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Mackup/.oh-my-zsh/custom
-    cd -
-}
 
 # Handy aliases
 alias ts="date -u '+%Y-%m-%dT%H:%M:%SZ'"
